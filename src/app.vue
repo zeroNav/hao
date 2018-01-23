b71049c8013af659b064e4338661586045205056
nooldey <openzws@163.com>
2018-01-23 16:15:55 +0800

    renew

--- src/app.vue
+++ src/app.vue
@@ -1,18 +1,21 @@
 <template>
-    <div class="z-parcel">
-        hello, {{ name }}
-        <p>
-            lallal
-        </p>
-    </div>
+    <col-panel>
+        <div slot="topbar">我是顶部栏</div>
+        <nav slot="navbar">我是侧边栏</nav>
+        <section slot="panel">我是主界面</section>
+    </col-panel>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            name: 'nooldey'
+    import ColPanel from './components/layout/col-panel.vue'
+    export default {
+        components: {
+            ColPanel
+        },
+        data() {
+            return {
+                name: 'nooldey'
+            }
         }
     }
-}
 </script>
---
+++ src/components/layout/col-panel.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layout">
+      <section class="top">
+          <slot name="topbar"></slot>
+      </section>
+      <section class="col-wrap">
+          <div class="nav">
+              <slot name="navbar"></slot>
+          </div>
+          <div class="panel">
+              <slot name="panel"></slot>
+          </div>
+      </section>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  width: 100vw;
+  height: 100vh;
+}
+.top {
+  height: 76px;
+  background-color: #fff;
+}
+.col-wrap {
+    width: 100%;
+    height: calc(100% - 76px);
+    display: flex;
+    justify-content: flex-start;
+    .nav {
+        flex: none;
+        width: 260px;
+        height: 100%;
+        overflow-y: auto;
+    }
+    .panel {
+        flex: 1;
+        height: 100%;
+        overflow-y: auto;
+    }
+}
+</style>
