a394ef44684ee80f7d4527142acf3b6356398f14
nooldey <openzws@163.com>
2018-01-23 14:50:40 +0800

    renew

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">hello, {{ name }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'nooldey'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .hello {
+        color: red;
+    }
+</style>
